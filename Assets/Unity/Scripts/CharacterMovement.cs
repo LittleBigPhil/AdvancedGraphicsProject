2e48aec964be1494b9f4613699e1c5f52eabd173
Phillip Roshon <philliproshon@gmail.com>
2021-04-08 10:47:20 -0400

    Player Movement
    Added instantaneous acceleration based movement.

---
+++ Assets/Unity/Scripts/CharacterMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float Speed = .2f;
+    public float Gravity = 10f;
+    private void FixedUpdate()
+    {
+        var controller = GetComponent<CharacterController>();
+        var v = Input.GetAxis("Vertical");
+        var h = Input.GetAxis("Horizontal");
+        var walking = Speed * (transform.forward * v + transform.right * h).normalized;
+        controller.Move(Time.fixedDeltaTime * (walking + Vector3.down * Gravity));
+    }
+}
